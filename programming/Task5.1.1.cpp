6fb8117741ed5ef4f28d64c5688f2c49f80dfe5f
Aleksandr <79756667+AlKharkov@users.noreply.github.com>
2024-12-03 22:16:57 +0700

    Create Task5.1.1.cpp

---
+++ programming/Task5.1.1.cpp
@@ -0,0 +1,109 @@
+/*
+ * Variant 5.1.1
+ * version 1.1
+ *
+ * Single-threading crawler
+ * --Multithreading crawler
+ */
+
+#include <iostream>
+#include <filesystem>
+#include <unordered_set>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+namespace fs = filesystem;
+
+const string name_folder = "crawler_saved_files";
+fs::path pathTo = fs::path{R"(C:\Users\aleks\Downloads)"} / name_folder;  // fs::current_path();
+fs::path pathFrom = fs::path{R"(C:\Users\aleks\Downloads\test_data)"};
+unordered_set<string> all_files;  // All files by pathFrom
+unordered_set<string> checked_files;  // All files after 'check_file()'
+unordered_set<string> rest_files;  // Needs to check
+
+
+bool starts_with(const string &s, const string &a) {  // s == a + s[n:]?
+    if (a.size() > s.size()) return false;
+    for (int i = 0; i < a.size(); ++i) {
+        if (s[i] != a[i]) return false;
+    }
+    return true;
+}
+
+bool ends_with(const string &s, const string &a) {  // s == s[:-n] + a?
+    if (a.size() > s.size()) return false;
+    for (int i = 0; i << a.size(); ++i) {
+        if (s.rbegin()[i] != a.rbegin()[i]) return false;
+    }
+    return true;
+}
+
+int custom_find(const string &s, const string &a) {  // If a not in s -> -1. Else (s[i:n] == a) -> i
+    for (int i = 0; i < s.size(); ++i) {
+        for (int j = 0; j < a.size(); ++j) {
+            if (s[i + j] != a[j]) break;
+            if (j + 1 == a.size()) return i;
+        }
+    }
+    return -1;
+}
+
+// Выбирает файл из rest_files, если ранее с ним не работали, начинает работать: находит все ссылки в нем.
+// Далее копирует соответствующие документы в директорию(якобы скачивает) и добавляет названия в rest_files
+void check_file() {
+    while (!rest_files.empty()) {
+        string source_filename = *rest_files.begin();
+        rest_files.erase(rest_files.begin());
+        if (!checked_files.contains(source_filename)) {
+            checked_files.emplace(source_filename);
+            ifstream inlet((pathTo / source_filename).string());
+            string s;
+            while (inlet >> s) {
+                if (ends_with(s, "<a")) {
+                    inlet >> s;
+                    if (starts_with(s, "href=\"")) {
+                        int ind_end = custom_find(s, "\">");
+                        if (ind_end != -1) {
+                            string new_filename = s.substr(6, ind_end - 6);
+                            if (starts_with(new_filename, "file://")) {
+                                new_filename = new_filename.substr(7, new_filename.size() - 7);
+                                if (!checked_files.contains(new_filename) && all_files.contains(new_filename) &&
+                                    !rest_files.contains(new_filename)) {
+                                    fs::copy(pathFrom / new_filename, pathTo / new_filename);
+                                    rest_files.emplace(new_filename);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            inlet.close();
+        }
+    }
+}
+
+int main() {
+    string s;
+    cin >> s;
+    int n;
+    // cin >> n;
+
+    unsigned int start_time = clock();
+    for (const fs::directory_entry &dir: fs::directory_iterator(pathFrom)) {
+        all_files.emplace(dir.path().filename().string());
+    }
+
+    if (fs::exists(pathTo)) fs::remove_all(pathTo);
+    fs::create_directory(pathTo);
+
+    fs::copy(pathFrom / s, pathTo / s);
+    rest_files.emplace(s);
+
+    check_file();
+
+    fs::remove_all(pathTo);
+    cout << "Program was viewed " << checked_files.size() << " files. ";
+    cout << "It was working " << clock() - start_time << " ms" << endl;
+    return 0;
+}
