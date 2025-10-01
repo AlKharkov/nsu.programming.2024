b54ea8269b90ad13ea4178dd9e5b4d2ad6e938e8
Aleksandr <79756667+AlKharkov@users.noreply.github.com>
2025-10-01 20:44:56 +0700

    Create 5term2.go

---
+++ Computational.practise/5term2.go
@@ -0,0 +1,148 @@
+/*
+Computational practice. Task 2; variant 19
+*/
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+)
+
+func main() {
+	h, tau := 0.1, 0.1
+	Nh, Nt := int(1/h)+1, int(1/tau)+1
+	// Input data
+	ux0 := func(x float64) float64 { return (x+0.1)*(x+0.1) + x }
+	u0t := func(t float64) float64 { return 0.1*0.1 - math.Sin(2*math.Pi*t)/2 - 3.5*t }
+	cxt := func(x, t float64) float64 { return (math.Pi*math.Cos(2*math.Pi*t) + 3.5) / (2*x + 1.2) }
+	uAns := func(x, t float64) float64 { return (x+0.1)*(x+0.1) - math.Sin(2*math.Pi*t)/2 + x - 3.5*t }
+	kToX := func(k int) float64 { return float64(k) * h }
+	jToT := func(j int) float64 { return float64(j) * tau }
+	fToGrid := func(fn func(float64, float64) float64, k, j int) float64 { return fn(kToX(k), jToT(j)) }
+	// Start solution
+	uGrid := make([][]float64, Nh)
+	for k := range Nh {
+		uGrid[k] = make([]float64, Nt)
+		uGrid[k][0] = ux0(kToX(k))
+	}
+	for j := range Nt {
+		uGrid[0][j] = u0t(jToT(j))
+	}
+	// Start calculating ujk
+	var vector []float64
+	matrix := make([][]float64, Nh-2)
+	for j := range Nt - 1 {
+		for i := range Nh - 2 {
+			matrix[i] = make([]float64, Nh-1)
+		}
+		vector = make([]float64, Nh-2)
+		for k := 1; k < Nh-1; k++ {
+			if k == 1 {
+				p := tau * fToGrid(cxt, k, j) / 4 / h
+				vector[0] = uGrid[k][j] + (uGrid[k-1][j+1]+uGrid[k+1][j]-uGrid[k-1][j])*p
+				matrix[0][0] = 1
+				matrix[0][1] = p
+			} else { // 1 < k < Nh-1
+				p := 4 * h / tau / fToGrid(cxt, k, j)
+				vector[k-1] = uGrid[k-1][j] - uGrid[k+1][j] - p*uGrid[k][j]
+				matrix[k-1][k-2] = 1
+				matrix[k-1][k-1] = -p
+				matrix[k-1][k] = -1
+			}
+		}
+		// Now we have 3-diagonal matrix and vector so that Au=b
+		showMatrix(matrix)
+		fmt.Println(vector)
+		for i, v := range runThroughMethod(matrix, vector) {
+			uGrid[i+1][j+1] = v
+		}
+		return
+	}
+	// Calculating maximum deviation between uGrid & u
+	var maximumDeviation float64
+	for j := range Nt {
+		for k := range Nh {
+			maximumDeviation = max(maximumDeviation, math.Abs(uGrid[k][j]-fToGrid(uAns, k, j)))
+		}
+	}
+	fmt.Println("--------Task 1--------")
+	fmt.Printf("Maximum deviation: %f\n\n", maximumDeviation)
+	fmt.Printf("%.2e ", maximumDeviation)
+	fmt.Println(maximumDeviation)
+
+	fmt.Println("--------Task 2--------")
+	// showMatrix(uGrid)
+	showMatrix(make([][]float64, 0))
+	fmt.Println()
+
+	fmt.Println("--------Task 3--------")
+	step := 0.01
+	iN := int(math.Round(1/step)) + 1
+	args := make([]float64, iN)
+	values := make([]opts.LineData, iN)
+	//values1 := make([]opts.LineData, iN)
+	//values2 := make([]opts.LineData, iN)
+	for i := range iN {
+		args[i] = round(step*float64(i), 6)
+		values[i] = opts.LineData{Value: math.Abs(uGrid[Nh/2][int(math.Round(float64(i)*step/tau))] - uAns(0.5, float64(i)*step))}
+		//values1[i] = opts.LineData{Value: uGrid[Nh/2][int(math.Round(float64(i)*step/tau))]}
+		//values2[i] = opts.LineData{Value: gridFunc(u, Nh/2, int(math.Round(float64(i)*step/tau)))}
+	}
+	chart := charts.NewLine()
+	chart.SetGlobalOptions(charts.WithTitleOpts(opts.Title{Title: "Decision error"}))
+	chart.SetXAxis(args).AddSeries("deviation", values)
+	//chart.SetXAxis(args).AddSeries("uGrid", values1)
+	//chart.SetXAxis(args).AddSeries("uExact", values2)
+	file, _ := os.Create("task3.html")
+	defer file.Close()
+	chart.Render(file)
+	fmt.Println("Successful! Look at the file <task3.html>")
+
+	runThroughMethod(make([][]float64, 0), make([]float64, 0))
+}
+
+func showMatrix(m [][]float64) {
+	for _, v := range m {
+		for _, u := range v {
+			if u != 0 {
+				fmt.Printf("%.0e\t", u)
+			} else {
+				fmt.Print("0\t")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func round(x float64, n int) float64 {
+	return math.Round(x*math.Pow10(n)) / math.Pow10(n)
+}
+
+// Ax = b. Find x
+func runThroughMethod(a [][]float64, b []float64) []float64 {
+	n := len(a)
+	//m := len(a[0])
+	for i := range n - 1 {
+		b[i+1] = b[i+1] - b[i]/a[i][i]
+		a[i+1][i+1] = a[i+1][i+1] - a[i][i+1]/a[i][i]
+		a[i+1][i] = 0
+	}
+	showMatrix(a)
+	fmt.Println(b)
+	/*for i := range n - 1 {
+		b[i+1] = b[i+1] - b[i]*a[i+1][i]/a[i][i]
+		a[i+1][i+1] = a[i+1][i+1] - a[i][i+1]*a[i+1][i]/a[i][i]
+		a[i+1][i] = 0
+	}*/
+	x := make([]float64, n)
+	/*x[n-1] = b[n-1] / a[n-1][n-1]
+	for i := n - 2; i >= 0; i-- {
+		x[i] = (b[i] - x[i+1]*a[i][i+1]) / a[i][i]
+	}*/
+	return x
+}
