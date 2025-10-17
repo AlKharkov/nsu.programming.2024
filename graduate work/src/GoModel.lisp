2682ddf44b97c4716d6273c8a655ea53cbfb42b2
Aleksandr <79756667+AlKharkov@users.noreply.github.com>
2025-10-17 22:01:32 +0700

    Update and rename Model.lisp to GoModel.lisp

--- graduate work/src/GoModel.lisp
+++ graduate work/src/GoModel.lisp
@@ -1,5 +1,7 @@
 #|
 	Model of the Go language
+
+	Last edit: 17/10/2025
 |#
 
 ;;; Lexical elements
@@ -193,152 +195,150 @@
 (obj "type assertion" :at "type" "type")  ; .(Type)
 ; var x interface{} = 7    // x has dynamic type int and value 7
 ; i := x.(int)             // i has type int and value 7
-(obj "primary expr & arguments" :at "primary expr" "primary expression" :at "arguments" (listt "expression"))
+(obj "primary expr & arguments" :at "primary expr" "primary expression" :at "arguments" (listt "expression"))  ; math.Sin(2)
 ;;expressions
-(obj "expression" :union ("unary expression" "e1 & binary operator & e2"))
-(obj "e1 & binary operator & e2" :at "arg1" "expression" :at "binary operator" "binary operator" :at "arg2" "expression")
+(obj "expression" :union ("unary expression" "1 & binary operator & 2"))
+(obj "1 & binary operator & 2" :at 1 "expression" :at "binary operator" "binary operator" :at 2 "expression")
 (obj "unary expression" :union ("primary expression" "unary operator & unary expression"))
 (obj "unary operator & unary expression" :at "operator" "unary operator" :at "expression" "unary expression")
 ;;operators
 ;;unary operators
-(obj "unary operator" :union ("+x" "-x" "^x" "!b" "*p" "&x" "<-"))
-(obj "+x" :at "x" "expression")  ; +x == 5
-(obj "-x" :at "x" "expression")  ; -x == -1 * x
-(obj "^x" :at "x" "expression")  ; ^10(2) == 01(2)
-(obj "!b" :at "b" "expression")  ; !true == false
-(obj "*x" :at "x" "expression")  ; *x
-(obj "&x" :at "x" "expression")  ; &x
+(obj "unary operator" :union ("+1" "-1" "^1" "!1" "*1" "&1" "<-"))
+(obj "+1" :at 1 "expression")  ; +x == x
+(obj "-1" :at 1 "expression")  ; -x == -1 * x
+(obj "^1" :at 1 "expression")  ; ^10(2) == 01(2)
+(obj "!1" :at 1 "expression")  ; !true == false
+(obj "*1" :at 1 "expression")
+(obj "&1" :at 1 "expression")
 (obj "<-" :at "channel" "channel type")
 ;;binary operators
-(obj "binary operator" :union ("||" "&&" "rel operator" "add operator" "mul operator"))
-(obj "||" :at "arg1" "expression" :at "arg2" "expression")  ; true || false == false
-(obj "&&" :at "arg1" "expression" :at "arg2" "expression")  ; true && false == false
+(obj "binary operator" :union ("1||2" "1&&2" "rel operator" "add operator" "mul operator"))
+(obj "1||2" :at 1 "expression" :at 2 "expression")
+(obj "1&&2" :at 1 "expression" :at 2 "expression")
 ;;binary multiplication operators
-(obj "mul operator" :union ("x*y" "x/y" "x%y" "x<<y" "x>>y" "x&y" "x&^y"))
-(obj "x*y" :at "x" "expression" :at "y" "expression")  ; 5 * 10 == 50
-(obj "x/y" :at "x" "expression" :at "y" "expression")  ; 4 / 2 = 2.0
-(obj "x%y" :at "x" "expression" :at "y" "expression")  ; 5 % 2 == 1
-(obj "x<<y" :at "x" "expression" :at "y" "expression") ; 1 << 12 == 2 ** 12
-(obj "x>>y" :at "x" "expression" :at "y" "expression") ; 32 >> 2 == 2 ** 3
-(obj "x&y" :at "x" "expression" :at "y" "expression")  ; 1100(2) & 1010(2) == 1000(2)
-(obj "x&^y" :at "x" "expression" :at "y" "expression") ; 11001010(2) &^ 10101100(2) == 01000010(2)    // 1 <=> 1 &^ 0
+(obj "mul operator" :union ("1*2" "1/2" "1%2" "1<<2" "1>>2" "1&2" "1&^2"))
+(obj "1*2" :at 1 "expression" :at 2 "expression")
+(obj "1/2" :at 1 "expression" :at 2 "expression")
+(obj "1%2" :at 1 "expression" :at 2 "expression")
+(obj "1<<2" :at 1 "expression" :at 2 "expression")
+(obj "1>>2" :at 1 "expression" :at 2 "expression")
+(obj "1&2" :at 1 "expression" :at 2 "expression")
+(obj "1&^2" :at 1 "expression" :at 2 "expression") ; 11001010(2) &^ 10101100(2) == 01000010(2)    // 1 <=> 1 &^ 0
 ;;binary addition operators
-(obj "add operator" :union ("x+y" "x-y" "x|y" "x^y"))
-(obj "x+y" :at "x" "expression" :at "y" "expression")  ; 2 + 3 == 5
-(obj "x-y" :at "x" "expression" :at "y" "expression")  ; 2 - 3 == -1
-(obj "x|y" :at "x" "expression" :at "y" "expression")  ; 1100(2) | 1010(2) == 1110(2)
-(obj "x^y" :at "x" "expression" :at "y" "expression")  ; 1100(2) ^ 1010(2) == 0110(2)
+(obj "add operator" :union ("1+2" "1-2" "1|2" "1^2"))
+(obj "1+2" :at 1 "expression" :at 2 "expression")
+(obj "1-2" :at 1 "expression" :at 2 "expression")
+(obj "1|2" :at 1 "expression" :at 2 "expression")
+(obj "1^2" :at 1 "expression" :at 2 "expression")
 ;;relation operators
-(obj "rel operator" :union ("x==y" "x!=y" "unequality operator"))
-(obj "x==y" :at "x" "expression" :at "y" "expression")  ; x == 3 | 4 == 5 | x == y
-(obj "x!=y" :at "x" "expression" :at "y" "expression")  ; x != 5
-(obj "unequality operator" :union ("x<y" "x<=y" "x>y" "x>=y"))
-(obj "x<y" :at "x" "expression" :at "y" "expression")   ; x < 3 | 3 < 5 | x < y
-(obj "x<=y" :at "x" "expression" :at "y" "expression")  ; x <= 3 | 3 <= 4 | x <= y
-(obj "x>y" :at "x" "expression" :at "y" "expression")   ; x > 3 | 4 > 5 | x > y
-(obj "x>=y" :at "x" "expression" :at "y" "expression")  ; x >= 3 | 3 >= 4 | x >= y
+(obj "rel operator" :union ("1=2" "1!2" "unequality operator"))
+(obj "1==2" :at 1 "expression" :at 2 "expression")
+(obj "1!=2" :at 1 "expression" :at 2 "expression")
+(obj "unequality operator" :union ("1<2" "1<2" "1>2" "1>2"))
+(obj "1<2" :at 1 "expression" :at 2 "expression")
+(obj "1<=2" :at 1 "expression" :at 2 "expression")
+(obj "1>2" :at 1 "expression" :at 2 "expression")
+(obj "1>=2" :at 1 "expression" :at 2 "expression")
 
 ;;; Statements
 (obj "statement" :union ("declaration" "label stmt" "simple stmt" "go stmt" "return stmt" "break stmt" "continue stmt" "goto stmt" "fallthrough stmt" "block" "if stmt" "switch stmt" "select stmt" "for stmt" "defer stmt"))
-(obj "simple statement" :union ("empty stmt" "expression stmt" "send stmt" "x++ stmt | x-- stmt" "assignment stmt" "short var decl"))
+(obj "simple statement" :union ("empty stmt" "expression stmt" "send stmt" "1++" "1--" "assignment stmt" "short var decl"))
 (obj "empty stmt")  ; <doc> The empty statement does nothing
 (obj "label stmt" :at "label" "label" :at "statement" "statement")
 (obj "label" :at "name" "identifier")
 ; "label": "statement"
 ;   Error: log.Panic("error encountered")
-(obj "expression stmt" :union ("expression"))
+(obj "expression stmt" :at "expression" "expression")
 ; h(x+y)
 ; f.Close()
 (obj "send stmt" :at "channel" "expression" :at "message" "expression")
-(obj "x++ stmt | x-- stmt" :union ("x++ stmt" "x-- stmt"))
-(obj "x++ stmt" :at "x" "expression")  ; x += 1
-(obj "x-- stmt" :at "x" "expression")  ; x -= 1
+(obj "1++ stmt" :at "1" "expression")  ; x += 1
+(obj "1-- stmt" :at "1" "expression")  ; x -= 1
 ;;assignment statements
-(obj "assignment stmt" :at "location" (listt "expression") :at "operator" "assignment operators" :at "expression" (listt "expression"))
-(obj "assignment operators" :union ("x=y" "x+=y" "x-=y" "x|=y" "x^=y" "x*=y" "x/=y" "x%=y" "x<<=y" "x>>=y" "x&=y" "x&^=y"))
-(obj "x=y" :at "x" "expression" :at "y" "expression")
-(obj "x+=y" :at "x" "expression" :at "y" "expression")
-(obj "x-=y" :at "x" "expression" :at "y" "expression")
-(obj "x|=y" :at "x" "expression" :at "y" "expression")
-(obj "x^=y" :at "x" "expression" :at "y" "expression")
-(obj "x*=y" :at "x" "expression" :at "y" "expression")
-(obj "x/=y" :at "x" "expression" :at "y" "expression")
-(obj "x%=y" :at "x" "expression" :at "y" "expression")
-(obj "x<<=y" :at "x" "expression" :at "y" "expression")
-(obj "x>>=y" :at "x" "expression" :at "y" "expression")
-(obj "x&=y" :at "x" "expression" :at "y" "expression")
-(obj "x&^=y" :at "x" "expression" :at "y" "expression")
+(obj "assignment stmt" :union ("1=2" "1+=2" "1-=2" "1|=2" "1^=2" "1*=2" "1/=2" "1%=2" "1<<=2" "1>>=2" "1&=2" "1&^=2"))
+(obj "1=2" :at 1 (listt "expression") :at 2 (listt "expression"))
+(obj "1+=2" :at 1 (listt "expression") :at 2 (listt "expression"))
+(obj "1-=2" :at 1 (listt "expression") :at 2 (listt "expression"))
+(obj "1|=2" :at 1 (listt "expression") :at 2 (listt "expression"))
+(obj "1^=2" :at 1 (listt "expression") :at 2 (listt "expression"))
+(obj "1*=2" :at 1 (listt "expression") :at 2 (listt "expression"))
+(obj "1/=2" :at 1 (listt "expression") :at 2 (listt "expression"))
+(obj "1%=2" :at 1 (listt "expression") :at 2 (listt "expression"))
+(obj "1<<=2" :at 1 (listt "expression") :at 2 (listt "expression"))
+(obj "1>>=2" :at 1 (listt "expression") :at 2 (listt "expression"))
+(obj "1&=2" :at 1 (listt "expression") :at 2 (listt "expression"))
+(obj "1&^=2" :at 1 (listt "expression") :at 2 (listt "expression"))
 ;;if statement
-(obj "if stmt" :at "init" "simple stmt" :at "expression" "expression" :at "block" "block" :at "else" "if stmt | block")
-(obj "if stmt | block" :union ("if statement" "block"))
+(obj "if stmt" :at "init" "simple stmt" :at "condition" "expression" :at "then" "block" :at "else" "if stmt | statements")
+(obj "if stmt | statements" :union ("if statement" (listt "statement")))
 ; if x := f(); x < y {
 ; 	return x
 ; } else if x > z {
 ; 	return z
 ; }
 (obj "switch stmt" :union ("expr switch stmt" "type switch stmt"))
-(obj "expr switch stmt" :at "stmt" "simple stmt" :at "expression" "expression" :at "expr case clause")
-(obj "expr case clause" :at "case" (listt "expression") :at "stmt" (listt "statement"))
+(obj "expr switch stmt" :at "init" "simple stmt" :at "controlling expression" "expression" :at "cases" (listt "expr case clause"))
+(obj "expr case clause" :at "cases" (listt "expression") :at "statements" (listt "statement"))
 ; switch tag {
 ; case 0, 1, 2, 3: s1()
 ; case 4, 5, 6, 7: s2()
 ; default: s3()
 ; }
-(obj "type switch stmt" :at "stmt" "simple stmt" :at "type switch guard" "type switch guard" :at "type case clause" "type case clause")
-(obj "type switch guard" :at "identifier" "identifier" :at "expression" "primary expression")
-(obj "type case clause" :at "case" (listt "type") :at "stmt" (listt "statement"))
+(obj "type switch stmt" :at "init" "simple stmt" :at "guard" "type switch guard" :at "cases" (listt "type case clause"))
+(obj "type switch guard" :at "type variable" "identifier" :at "variable" "primary expression")
+(obj "type case clause" :at "cases" (listt "type") :at "statements" (listt "statement"))
 ; switch t := x.(type) {              // type checker
 ; case nil:
 ; 	fmt.Println("x is nil")  
 ; default:
 ;   fmt.Println("don't know the type")
-(obj "for stmt" :at "variety" "condition | for clause | range clause" :at "block" "block")
-(obj "condition | for clause | range clause" :union ("expression" "for clause" "range clause"))
-(obj "for clause" :at "init" "simple stmt" :at "condition" "expression" :at "post" "simple stmt")
-(obj "range clause" :at "location" "expr list | identifier list" :at "expression" "expression")
+(obj "for statement" :union ("for stmt with condition clause" "for stmt with range clause"))
+(obj "for stmt with condition clause" :at "init" "simple stmt" :at "condition" "expression" :at "post" "simple stmt" :at "body" "block")
+(obj "for stmt with range clause" :at "location" "expr list | identifier list" :at "expression" "expression" :at "body" "block")
 (obj "expr list | identifier list" :union ("expr list" "identifier list"))
-; for a < b { a *= 2 }                // the usual while loop
+; for x < y { a *= 2 }                // the usual while loop
 ; for i := 0; i < 10; i++ { f(i) }    // the usual for loop
 ; a := [5]int{1, 2, 3, 4, 5}
 ; for i, v := range a {
 ;   fmt.Println(i, v)                 // i a[i]
 ; }
-(obj "go stmt" :at "expression" "expression")  ; <doc> It starts the execution of a function call as an independent concurrent thread of control
+(obj "go stmt" :at "thread body" "expression")  ; <doc> It starts the execution of a function call as an independent concurrent thread of control
 ; go Server()
 ; go func(ch chan<- bool) { for { sleep(10); ch <- true }} (c)
-(obj "select stmt" :at "statement" "common slause")
+(obj "select stmt" :at "statement" (listt "common clause"))
 (obj "common clause" :at "case" "common case" :at "stmt list" (listt "statement"))
-(obj "common case" :union ("send stmt" "recv stmt"))
-(obj "recv stmt" :at "list" "expr list | identifier list" :at "expression" "expression")
-(obj "expr list | identifier list" :union ("expr list" "identifier list"))
-; go func(c1 chan int) {
+(obj "common case" :union ("send stmt" "recive stmt"))
+(obj "recive stmt" :at "list" "expr list | identifier list" :at "expression" "expression")
+; func f1(c1 chan int) {
 ; 	a := <-c1
-; 	c1 <- 20 * a
+; 	c1 <- a
 ; }
-; go func(c2 chan int) {
+; func f2(c2 chan int) {
 ; 	b := <-c2
-; 	c2 <- 100 * b
+; 	c2 <- b
 ; }
 ; c1, c2 := make(chan int), make(chan int)
-; c1 <- 2
-; c2 <- 3
+; go f1(c1)
+; go f2(c2)
+; c1 <- 3
+; c2 <- 4
 ; select {
 ; case a := <-c1:
 ;   fmt.Println(1, a)
 ; case b := <-c2:
 ;   fmt.Println(2, b)
 ; }
-(obj "return stmt" :at "expr list" "expr list")
-(obj "break stmt" :at "label" "label")
+(obj "return" :at "expressions" "expr list")
+(obj "break " :at "label" "label")
 ; OuterLoop:
 ;   for {
 ;     for {
 ;       break OuterLoop
 ;     }
 ;   }
-(obj "continue stmt" :at "label" "label")
-(obj "goto stmt" :at "label" "label")
-(obj "fallthrough stmt")
+(obj "continue" :at "label" "label")
+(obj "goto" :at "label" "label")
+(obj "fallthrough" :enum ("fallthrough"))
 ; switch {                     // prints "true & false"
 ;   case true:
 ;     fmt.Print("true & ")
@@ -353,20 +353,7 @@
 
 
 ;;; Source file organization
-(obj "source file" :at "package clause" "package clause" :at "import decl" "import decl" :at "top level decl" "top level decl")
-;;package clause
-(obj "package clause" :union ("package name"))  ; package math
-(obj "package name" :union ("identifier"))
+(obj "source file" :at "package" "identifier" :at "import decl" (listt "import package") :at "declarations" "top level decl")
 ;;import declaration
-(obj "import decl" :at "import spec list" (listt "import spec"))
-(obj "import spec" :at "name" "package name" :at "import path" "import path")
-(obj "import path" :union(string))  ; import m "lib/math"
-
-
-;;; For the future
-#|
-(obj "variable declaration" :at "name" "identifier" :at "type" "type" :at "value" "expression")  ; Need to check: (is-instance (aget variable "value") (aget variable "type"))
-(obj "array" :at "type" "array type" :at "value" (listt any))
-(obj "slice" :at "type" "slice type" :at "start index" nat :at "length" nat :at "capacity" nat)
-for slice decl :at "element type" "type" :at "start index" "nat" :at "len" nat :at "cap" nat
-|#
+(obj "import package" :at "name" "package name" :at "path" "path")
+(obj "path" :union(string))  ; import m "lib/math"
